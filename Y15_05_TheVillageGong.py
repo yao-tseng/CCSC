469f74f5154b888d0c0bf4dce726b786ae6e532a
Justin Lo <justinlo@bucketteer.com>
2017-10-01 11:16:07 -0400

    Update Y15_05_TheVillageGong.py

--- Y15_05_TheVillageGong.py
+++ Y15_05_TheVillageGong.py
@@ -1,12 +1,12 @@
 def villageGong():
-    testCases = int(input("Number of Test Cases: "))
-    i = 0
-    while i < testCases:
-        timeInput = input("Test Case " + str(i+1) + ": ")
-        HMS = [int(s) for s in timeInput.split(":")]
-        totalSeconds = (HMS[1] * 60) + HMS[2]
-        secondsLeft = 600 - (totalSeconds % 600)
-        print(secondsLeft)
-        i = i + 1
+    testCases = int(input("Number of Test Cases: "))        #input for number of test cases
+    i = 0                                                   #set index
+    while i < testCases:                                    #iterate for each test case
+        timeInput = input("Test Case " + str(i+1) + ": ")   #input time for each test case
+        HMS = [int(s) for s in timeInput.split(":")]        #transform str input into int array
+        totalSeconds = (HMS[1] * 60) + HMS[2]               #find total seconds from minutes + seconds
+        secondsLeft = 600 - (totalSeconds % 600)            #find remaining seconds out of 10 minutes
+        print(secondsLeft)                                  #output seconds
+        i = i + 1                                           #advance index
         
 villageGong()
